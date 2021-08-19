8b4e38297f969772903c9a28bcf0512a170708af
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-19 23:56:50 +0600

    Finished pin matcher

---
+++ js/script.js
@@ -0,0 +1,45 @@
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+    }
+}
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calInput.value = '';
+        }
+    }
+    else {
+        const previousNumber = calInput.value;
+        const newNumber = previousNumber + number;
+        calInput.value = newNumber;
+    }
+});
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+    if (pin == typedNumbers) {
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else {
+        successMessage.style.display = 'none';
+        failError.style.display = 'block';
+    }
+}
